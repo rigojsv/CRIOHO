a1f68ec4bba4b64e879fd5144fd259add98f012a
rigojsv <rigojsv.rsv@gmail.com>
2024-11-19 04:11:21 -0600

    modified:   js/main.js 	new file:   less/about.less 	new file:   less/carousel.less 	new file:   less/contact.less 	new file:   less/events.less 	modified:   less/main.less 	new file:   less/services.less 	new file:   less/sponsor.less 	modified:   nosotros.html 	modified:   partials/carousel.hbs 	modified:   partials/header.hbs 	modified:   resources/img/Logo.png

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 import '../less/main.less';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const mySlider = new SliderController();
+
   // Mobile menu toggle
   const navLinks = document.querySelector('.nav-links');
   const hamburger = document.createElement('button');
@@ -52,3 +54,104 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+
+
+class SliderController {
+  slider = null;
+  slides = [];
+  framestack = null;
+  intervalTime = 3000;
+  intervalId = null;
+  currentSlide = -1;
+  slideDirection = 1;
+
+  constructor() {
+      this.slider = document.querySelector('.slider');
+      this.framestack = document.querySelector('.frametrack');
+      this.slides = [...document.querySelectorAll('.frametrack .slide')];
+
+      if (!this.slider || !this.framestack || this.slides.length === 0) {
+          console.error("Estructura del slider no encontrada.");
+          return;
+      }
+
+      this.currentSlide = 0;
+      this.generateUI();
+      this.moveToSlide(0);
+  }
+
+  moveToSlide(slideIndex) {
+      if (this.intervalId) {
+          clearTimeout(this.intervalId);
+      }
+      this.currentSlide = slideIndex;
+      this.framestack.style.left = `-${this.currentSlide * 100}vw`;
+      this.updateNavigation();
+      this.tick();
+  }
+
+  tick() {
+      this.intervalId = setTimeout(() => {
+          this.moveToNext();
+      }, this.intervalTime);
+  }
+
+  moveToNext() {
+      if (this.currentSlide + this.slideDirection >= this.slides.length || this.currentSlide + this.slideDirection < 0) {
+          this.slideDirection *= -1;
+      }
+      this.currentSlide += this.slideDirection;
+      this.moveToSlide(this.currentSlide);
+  }
+
+
+  updateNavigation() {
+    const navigationIndexes = document.querySelectorAll('.navigation-index');
+    navigationIndexes.forEach((nav, i) => {
+        nav.classList.toggle('active', i === this.currentSlide);
+    });
+}
+
+  generateUI() {
+      let btnDerecha = document.createElement("div");
+      btnDerecha.innerHTML = '&gt;';
+      btnDerecha.classList.add('navigation-btn', 'navigate-right');
+      btnDerecha.addEventListener('click', (e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          this.slideDirection = 1;
+          this.moveToNext();
+      });
+
+      let btnIzquierda = document.createElement("div");
+      btnIzquierda.innerHTML = '&lt;';
+      btnIzquierda.classList.add('navigation-btn', 'navigate-left');
+      btnIzquierda.addEventListener('click', (e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          this.slideDirection = -1;
+          this.moveToNext();
+      });
+
+      this.slider.appendChild(btnIzquierda);
+      this.slider.appendChild(btnDerecha);
+
+      let contenedorNavegacion = document.createElement("div");
+      contenedorNavegacion.classList.add('navigation-container');
+
+      this.slides.forEach((_o, i) => {
+          let slideNavigate = document.createElement('div');
+          slideNavigate.classList.add('navigation-index');
+          slideNavigate.addEventListener('click', (e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              this.moveToSlide(i);
+          });
+          contenedorNavegacion.appendChild(slideNavigate);
+      });
+
+      this.slider.appendChild(contenedorNavegacion);
+      this.updateNavigation();
+  }
+}
